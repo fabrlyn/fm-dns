771f18c522d24c16ad91ffa546e83e533bc52d3f
fabrlyn <robert.barlin@gmail.com>
2024-02-07 21:00:21 +0100

    Clean up scanner model

--- fm_dns/src/scanner/model.rs
+++ fm_dns/src/scanner/model.rs
@@ -10,12 +10,15 @@ pub struct Response {
     pub responded_at: chrono::DateTime<Utc>,
 }
 
-#[derive(Debug, Clone, Serialize)]
-pub enum Class {
-    IN = 1,
-    CS = 2,
-    CH = 3,
-    HS = 4,
+impl Response {
+    pub fn from_mdns_response(responded_at: DateTime<Utc>, response: mdns::Response) -> Self {
+        Self {
+            responded_at,
+            additional: response.additional.into_iter().map(Into::into).collect(),
+            answers: response.answers.into_iter().map(Into::into).collect(),
+            nameservers: response.nameservers.into_iter().map(Into::into).collect(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize)]
@@ -26,7 +29,38 @@ pub struct Record {
     pub kind: RecordKind,
 }
 
+impl From<mdns::Record> for Record {
+    fn from(value: mdns::Record) -> Self {
+        Self {
+            name: value.name,
+            class: value.class.into(),
+            ttl: value.ttl,
+            kind: value.kind.into(),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Serialize)]
+pub enum Class {
+    IN,
+    CS,
+    CH,
+    HS,
+}
+
+impl From<dns_parser::Class> for Class {
+    fn from(value: dns_parser::Class) -> Self {
+        match value {
+            dns_parser::Class::IN => Self::IN,
+            dns_parser::Class::CS => Self::CS,
+            dns_parser::Class::CH => Self::CH,
+            dns_parser::Class::HS => Self::HS,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize)]
+#[serde(tag = "tag", content = "content")]
 pub enum RecordKind {
     A(Ipv4Addr),
     AAAA(Ipv6Addr),
@@ -45,28 +79,6 @@ pub enum RecordKind {
     TXT(Vec<String>),
     PTR(String),
     Unimplemented(Vec<u8>),
-}
-
-impl From<mdns::Record> for Record {
-    fn from(value: mdns::Record) -> Self {
-        Self {
-            name: value.name,
-            class: value.class.into(),
-            ttl: value.ttl,
-            kind: value.kind.into(),
-        }
-    }
-}
-
-impl From<dns_parser::Class> for Class {
-    fn from(value: dns_parser::Class) -> Self {
-        match value {
-            dns_parser::Class::IN => Self::IN,
-            dns_parser::Class::CS => Self::CS,
-            dns_parser::Class::CH => Self::CH,
-            dns_parser::Class::HS => Self::HS,
-        }
-    }
 }
 
 impl From<mdns::RecordKind> for RecordKind {
@@ -97,17 +109,6 @@ impl From<mdns::RecordKind> for RecordKind {
             mdns::RecordKind::TXT(r) => Self::TXT(r),
             mdns::RecordKind::PTR(r) => Self::PTR(r),
             mdns::RecordKind::Unimplemented(r) => Self::Unimplemented(r),
-        }
-    }
-}
-
-impl Response {
-    pub fn from_mdns_response(responded_at: DateTime<Utc>, response: mdns::Response) -> Self {
-        Self {
-            responded_at,
-            additional: response.additional.into_iter().map(Into::into).collect(),
-            answers: response.answers.into_iter().map(Into::into).collect(),
-            nameservers: response.nameservers.into_iter().map(Into::into).collect(),
         }
     }
 }
