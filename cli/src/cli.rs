20efd134e5638ea9d72ac562a266e5054b38aa4e
fabrlyn <robert.barlin@gmail.com>
2024-02-08 20:11:22 +0100

    Update documentation

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -14,7 +14,7 @@ struct Cli {
         value_parser = parse_service_query 
     )]
     service_query: Arc<ServiceQuery>,
-    #[arg(help = "The interval to query the network.", default_value = "60", value_parser = parse_interval, long="interval")]
+    #[arg(help = "The interval in seconds to query the network.", default_value = "60", value_parser = parse_interval, long="interval")]
     interval: Duration,
 }
 
---
+++ readme.md
@@ -0,0 +1,18 @@
+# fm-dns
+
+Command line tool for querying mdns services. 
+
+Outputs the response in json.
+
+## usage
+
+```sh
+$ fm-dns --interval 10 _googlecast._tcp.local 
+```
+
+## install 
+
+```sh
+$ make install
+```
+
